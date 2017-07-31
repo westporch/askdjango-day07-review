94686ce5fd59ad94bce9d5e758d91c6b60d7b0c3
Westporch <westporch@gmail.com>
2017-07-31 17:56:35 +0900

    accounts.views.profile 뷰 구현

--- dev/askdjango/accounts/urls.py
+++ dev/askdjango/accounts/urls.py
@@ -2,7 +2,14 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from . import views
 
+'''url(r'^login/$', auth_views.login, name='login'),에 대한 설명
+예를 들어 http://192.168.0.7:8080/accounts/login/ 접속시 프로파일 페이지로 이동한다.
+장고 로그인 폼에서는 로그인 시 기본적으로 프로파일 페이지로 이동하게 끔 되어있다.
+next를 이용하면 로그인 후 이동할 페이지를 지정할 수 있다.
+예) http://127.0.0.1:8080/accounts/login/?next=/blog/
+'''
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'), # 예) http://192.168.0.7:8080/accounts/login/
+    url(r'^profile/$', views.profile, name='profile'),
 ]
