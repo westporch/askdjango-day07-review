35eb83f2211b77f845f064a8b6f8bffdd53b85c8
Westporch <westporch@gmail.com>
2017-07-31 17:38:10 +0900

    로그인 구현

--- dev/askdjango/accounts/urls.py
+++ dev/askdjango/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from . import views
 
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'), # 예) http://192.168.0.7:8080/accounts/login/
 ]
