7ef54bffeb3e7ac706c71255be9af2ffd9c91064
Westporch <westporch@gmail.com>
2017-07-31 13:01:47 +0900

    FileSystem Static Finder 설정

--- dev/askdjango/askdjango/settings.py
+++ dev/askdjango/askdjango/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askdjango', 'static')
+]
+
 
 ''' django-debug-toolbar에 접속할 호스트 설정
 --> settings.py에 INTERNAL_IPS를 설정하면 된다.
