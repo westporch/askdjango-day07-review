e77c6ab1590e3bb826dd207e681a86f19434b544
Westporch <westporch@gmail.com>
2017-08-01 10:37:36 +0900

    로그인 answer필드에 대한 유효성 검사를 clean_ 에서 수행

--- dev/askdjango/accounts/forms.py
+++ dev/askdjango/accounts/forms.py
@@ -6,11 +6,18 @@ class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
 
-
+'''
 def answer_validator(value):
     if value != 6:
         raise forms.ValidationError('올바른 값을 입력해주세요.')
+'''
 
 
 class LoginForm(AuthenticationForm):
-    answer = forms.IntegerField(help_text='3 + 3 = ?', validators=[answer_validator])
+    answer = forms.IntegerField(help_text='3 + 3 = ?')
+
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer', None)
+        if answer != 6:
+            raise forms.ValidationError('올바른 CAPTCHA 값을 입력해주세요.')
+        return answer
