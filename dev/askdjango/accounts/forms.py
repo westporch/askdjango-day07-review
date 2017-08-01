a22d77c0a21b473b8a8a56379f12d7126c510a44
Westporch <westporch@gmail.com>
2017-08-01 10:12:59 +0900

    로그인시 캡챠 기능을 추가함

--- dev/askdjango/accounts/forms.py
+++ dev/askdjango/accounts/forms.py
@@ -1,6 +1,16 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
+
+
+def answer_validator(value):
+    if value != 6:
+        raise forms.ValidationError('올바른 값을 입력해주세요.')
+
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(help_text='3 + 3 = ?', validators=[answer_validator])
--- dev/askdjango/accounts/urls.py
+++ dev/askdjango/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 from . import views
+
 
 '''url(r'^login/$', auth_views.login, name='login'),에 대한 설명
 예를 들어 http://192.168.0.7:8080/accounts/login/ 접속시 프로파일 페이지로 이동한다.
@@ -12,7 +14,10 @@ next를 이용하면 로그인 후 이동할 페이지를 지정할 수 있다.
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login',
-        kwargs={'template_name': 'accounts/login.html'}), # 예) http://192.168.0.7:8080/accounts/login/
+        kwargs={
+            'template_name': 'accounts/login.html', # 예) http://192.168.0.7:8080/accounts/login/
+            'authentication_form': LoginForm,
+        }),
     url(r'^logout/$', auth_views.logout, name='logout',
         kwargs={'next_page': settings.LOGIN_URL}),
     url(r'^profile/$', views.profile, name='profile'),
